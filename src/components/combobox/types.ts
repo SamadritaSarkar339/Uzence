0565144fc7336a171e3e676862480b960c4e56be
SamadritaSarkar339 <samadritasarkar1@gmail.com>
2025-10-26 19:07:08 +0530

    Update types.ts

--- src/components/combobox/types.ts
+++ src/components/combobox/types.ts
@@ -1,1 +1,18 @@
-export type Option = string; export interface ComboboxMultiProps { label: string; options: Option[]; allowCreate?: boolean; }
+export type Option = string;
+
+export interface ComboboxMultiProps {
+  /** Unique ID for combobox container (optional) */
+  id?: string;
+
+  /** Label displayed above the combobox */
+  label: string;
+
+  /** List of options to display */
+  options: Option[];
+
+  /** Placeholder text shown in the input */
+  placeholder?: string;
+
+  /** Enables creation of new options */
+  allowCreate?: boolean;
+}
